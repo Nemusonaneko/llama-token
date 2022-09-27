5c00123f339d81aaa29d85b30f2a1922a717f7b3
nemusona <nemusonaneko@protonmail.com>
2022-09-26 19:17:27 -0500

    change burn

--- src/LlamaToken.sol
+++ src/LlamaToken.sol
@@ -9,7 +9,7 @@ contract LlamaToken is ERC20("LlamaToken", "LLAMA", 18) {
         _mint(_to, _amount);
     }
 
-    function burn(address _from, uint256 _amount) public {
-        _burn(_from, _amount);
+    function burn(uint256 _amount) public {
+        _burn(msg.sender, _amount);
     }
 }
